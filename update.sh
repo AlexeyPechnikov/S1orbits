1e49c91308561194c06e90b62f93fce5cb177f19
Alexey Pechnikov <pechnikov@mobigroup.ru>
2024-03-25 00:34:42 +0700

    Update CI workflow and update script

--- update.sh
+++ update.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 set -e
 
+# determine OS and execute the respective date commands
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    MONTHS="$(date '+%Y-%m') $(date -v-1m '+%Y-%m')"
+else
+    MONTHS="$(date '+%Y-%m') $(date '+%Y-%m' --date='1 month ago')"
+fi
+
 for SATELLITE in S1A S1B
 do
     for ORBIT in POEORB RESORB
     do
-        for DATE in $(date "+%Y-%m") $(date -v-1m "+%Y-%m")
+        for MONTH in $MONTHS
         do
-            python3 ./dload.py . ${SATELLITE} ${ORBIT} ${DATE} &
+            python3 ./dload.py . ${SATELLITE} ${ORBIT} ${MONTH} &
         done
         wait
     done
