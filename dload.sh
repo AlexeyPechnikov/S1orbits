6b93f9c36b2a06c6500614a5099856fceb1f9473
Alexey Pechnikov <pechnikov@mobigroup.ru>
2024-03-25 11:47:46 +0700

    Detect current year automatically in the download script and use bash.

--- dload.sh
+++ dload.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+TODAY=$(date '+%Y')
 
 for SATELLITE in S1A S1B
 do
     for ORBIT in POEORB RESORB
     do
-        for YEAR in $(seq 2014 2024)
+        for YEAR in $(seq 2014 "$TODAY")
         do
             python3 ./dload.py . ${SATELLITE} ${ORBIT} ${YEAR} &
         done
