07dba506a88f1bf40248fcac4e8bcb0ca852b856
Alexey Pechnikov <pechnikov@mobigroup.ru>
2025-04-01 14:08:12 +0700

    Add S1C satellite orbits handling

--- dload.sh
+++ dload.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-set -e
+#set -e
 
 TODAY=$(date '+%Y')
 
-for SATELLITE in S1A S1B
+for SATELLITE in S1A S1B S1C
 do
     for ORBIT in POEORB RESORB
     do
         for YEAR in $(seq 2014 "$TODAY")
         do
-            python3 ./dload.py . ${SATELLITE} ${ORBIT} ${YEAR} &
+            #python3 ./dload.py . ${SATELLITE} ${ORBIT} ${YEAR} &
+            python3 ./dload.py . ${SATELLITE} ${ORBIT} ${YEAR} || echo "ERR: ${SATELLITE} ${ORBIT} ${YEAR}" &
         done
         wait
     done
--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+#set -e
 
 # determine OS and execute the respective date commands
 if [[ "$OSTYPE" == "darwin"* ]]
@@ -9,13 +9,14 @@ else
     MONTHS="$(date '+%Y-%m') $(date '+%Y-%m' --date='1 month ago')"
 fi
 
-for SATELLITE in S1A S1B
+for SATELLITE in S1A S1B S1C
 do
     for ORBIT in POEORB RESORB
     do
         for MONTH in $MONTHS
         do
-            python3 ./dload.py . ${SATELLITE} ${ORBIT} ${MONTH} &
+            #python3 ./dload.py . ${SATELLITE} ${ORBIT} ${MONTH} &
+            python3 ./dload.py . ${SATELLITE} ${ORBIT} ${MONTH} || echo "ERR: ${SATELLITE} ${ORBIT} ${MONTH}" &
         done
         wait
     done
